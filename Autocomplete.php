6bbbc2eb54eeb42a6db3e64fad22ed28003e4cff
korshunovmaxim <korshunov.m.e@gmail.com>
2016-11-02 22:44:20 +0500

    asset + widget

---
+++ Autocomplete.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace maxeko\devbridge;
+
+use yii\base\Widget;
+
+/**
+ * Devbridge jQuery Autocomplete Widget
+ * https://www.devbridge.com/sourcery/components/jquery-autocomplete/
+ *
+ * @author Maxim Korshunov <korshunov.m.e@gmail.com>
+ */
+class Autocomplete extends Widget
+{
+    public $inputSelector = 'input';
+    public $options = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        $view = $this->getView();
+
+        AutocompleteAsset::register($this->view);
+
+        $jsOptions = [];
+        foreach ($this->options as $key => $value) {
+            $jsOptions[] = "{$key}:{$value}";
+        }
+
+        $jsOptions = join(',', $jsOptions);
+        $view->registerJs("$({$this->inputSelector}).autocomplete({{$jsOptions}});");
+    }
+}
---
+++ AutocompleteAsset.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace maxeko\devbridge;
+
+use yii\web\AssetBundle;
+
+/**
+ * Devbridge jQuery Autocomplete Asset
+ *
+ * @package maxeko\devbridge
+ */
+class AutocompleteAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/devbridge/jquery-autocomplete';
+    public $depends = [
+        'yii\web\JqueryAsset'
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->js[] = YII_ENV_DEV ? 'jquery.autocomplete.js' : 'jquery.autocomplete.min.js';
+    }
+}
